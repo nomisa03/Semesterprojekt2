a4d8c014eacf1fc742e5dad024e68249b7761c95
nomisa03 <nomisa03@gmail.com>
2024-04-22 14:19:07 +0200

    Planen er lagt

--- Database.py
+++ Database.py
@@ -1,7 +1,35 @@
 import sqlite3
+from datetime import datetime
+
+now = datetime.now()
 
 conn = sqlite3.connect('database.db')
 
-conn.execute("CREATE TABLE()")
+cur = conn.cursor()
 
-conn.execute("")
+data = [
+(now), ("Rum 1"), (28) ,("Person i rummet"), ("Giver varm luft")
+]
+
+cur.execute("CREATE TABLE IF NOT EXISTS Data(Timestamp, Room, Temperatur, Aktvitet, Statusrum)")
+
+cur.execute("INSERT INTO Data VALUES(? , ? , ? , ? , ?)", data)
+
+conn.commit()
+
+
+
+def Readsystem()
+    #Læs I2C fra data harvester
+    #while read = true
+    cur.execute("INSERT INTO Data VALUES(? , ? , ? , ? , ?)", data)
+    return 0
+
+
+
+#def makegraph()
+    #cur.execute(Read)
+    #read = grahp
+    #Matplotlib
+    #Send til GUI
+    #return graph
