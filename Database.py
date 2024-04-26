62979aca2722868d2907a622d8480d2ccd4b63fc
nomisa03 <nomisa03@gmail.com>
2024-04-26 11:20:15 +0200

    Big update

--- Database.py
+++ Database.py
@@ -1,36 +1,53 @@
 import sqlite3
 from datetime import datetime
+import matplotlib as plt
+from matplotlib import gridspec
 
 now = datetime.now()
+
+import serial
+
+ser = serial.Serial(
+    port='COM5',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+
+ser.close()
 
 conn = sqlite3.connect('database.db')
 
 cur = conn.cursor()
 
-data = [
-(now), ("Rum 1"), (28) ,("Person i rummet"), ("Giver varm luft")
-]
+#data = [
+#(now), ("Rum 1"), (28) ,("Person i rummet"), ("Giver varm luft")
+#]
 
-cur.execute("CREATE TABLE IF NOT EXISTS Data(Timestamp, Room, Temperatur, Aktvitet, Statusrum)")
-
-cur.execute("INSERT INTO Data VALUES(? , ? , ? , ? , ?)", data)
-
-conn.commit()
-
-
+cur.execute("CREATE TABLE IF NOT EXISTS Rum1(Timestamp, Room, Temperatur, Aktvitet, Statusrum)")
+cur.execute("CREATE TABLE IF NOT EXISTS Rum2(Timestamp, Room, Temperatur, Aktvitet, Statusrum)")
 
 def Readsystem()
-    #Læs I2C fra data harvester
-    #while read = true
-    #læst data
-    #Vent 10sek
-    #Hvis requst = true 
-    #makegraph()
-    #break
+    incomming.split(":",)
+    cur.execute("INSERT INTO Rum1 VALUES(? , ? , ? , ? , ?)", incomming)
+    print("Data succsesfu in table Rum 1")
+    conn.commit
+    if incomming == 2():
+    try 
+        cur.execute("INSERT INTO Rum2 VALUES(? , ? , ? , ? , ?)", incomming)
+        print("Data succsesfu in table Rum 2")
+        conn.commit
+    else
+        return 0
 
-    cur.execute("INSERT INTO Data VALUES(? , ? , ? , ? , ?)", data)
-    return 0
 
+def ReadUART()
+    while True:
+        for line in ser.read():
+            incomming = str(count) + str(': ') + chr(line)
+            count = count+1
 
 
 #def makegraph()
