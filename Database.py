5d1f63c63eefac75590ba0bd0fe885e18ca2c46c
nomisa03 <nomisa03@gmail.com>
2024-05-07 10:48:08 +0200

    Updated the list split

--- Database.py
+++ Database.py
@@ -1,9 +1,13 @@
 import sqlite3
-#from datetime import datetime
+from datetime import datetime
 import serial
 from serial import Serial
+splitc = ";"
+incomming = []
 
-#now = datetime.now() #For making a timestamp of current time
+#['M:1;S:48:T:31.20:M:1;S:49:T:1.20:M:1;\r\n']
+
+now = datetime.now() #For making a timestamp of current time
 
 conn = sqlite3.connect('database.db') #connet to local database in file system
 cur = conn.cursor() #cursoer for looking in database
@@ -23,20 +27,30 @@ ser = serial.Serial(
     timeout=0)
 
 # Aktivitet høj = 1, lav = 0. Statusrum -1 = afgiver varme, 0 = neutral, 1 = får varme
-cur.execute("CREATE TABLE IF NOT EXISTS Rum1(Timestamp, Temperatur, Aktivitet, Statusrum)") 
-cur.execute("CREATE TABLE IF NOT EXISTS Rum2(Timestamp, Temperatur, Aktivitet, Statusrum)")
+cur.execute("CREATE TABLE IF NOT EXISTS Rum1(Timestamp, Temperatur, Aktivitet)")
+cur.execute("CREATE TABLE IF NOT EXISTS Rum2(Timestamp, Temperatur, Aktivitet)")
 
 def Readsystem(incomming):
-    rooms = 0
-    incomming.split(";",) # so list looks like this incommin = [Rum1; 28 , ("1") : Rum2, 28, ("0")]
-    string_count = len(incomming)
-    rum1 = incomming[1].split(":")
-    #rum1.insert(0, now)
-    rum2 = incomming[2].split(":")
-    #rum2.insert(0, now)
+    rooms = 2
+    #incomming.decode(encoding='UTF-8')
+    incomming.split(";") # so list looks like this incommin = [Rum1; 28 , ("1") : Rum2, 28, ("0")]
+    #string_count = len(incomming)
+    rum1 = incomming[1].split(splitc)
+    print(rum1)
+    #rum1.pop(0)
+    #rum1.pop(1)
+    #rum1.pop(2)
+    #rum1.pop(4)
+    rum1.insert(0, now)
+    rum2 = incomming[2].split(splitc)
+    #rum2.pop(0)
+    #rum2.pop(1)
+    #rum2.pop(2)
+    #rum2.pop(4)
+    rum2.insert(0, now)
     if len(rum1) == 4:
         try:
-            cur.execute("INSERT INTO Rum1 VALUES(? , ? , ? , ?)",rum1)
+            cur.execute("INSERT INTO Rum1 VALUES(? , ? , ?)",rum1)
             print("Data succsesfu in table Rum 1")
             conn.commit
         except:
@@ -45,7 +59,7 @@ def Readsystem(incomming):
     
     if len(rum2) == 4:
         try:
-            cur.execute("INSERT INTO Rum2 VALUES(? , ? , ? , ?)",rum2)
+            cur.execute("INSERT INTO Rum2 VALUES(? , ? , ?)",rum2)
             print("Data succsesfu in table Rum 2")
             conn.commit
             main()
@@ -56,30 +70,33 @@ def Readsystem(incomming):
 
 
 def main():
-    while True:
-        ser.open()
-        bytesToRead = conn.in_waiting
-        print(bytesToRead)
-        if (bytesToRead > 3):
-            incomming = conn.read(bytesToRead)
-            print(incomming)
-            Readsystem(incomming)
-            conn.reset_input_buffer()
-            #incomming = port.readline()
-            #print(incomming)
-            ser.close()
-
-
-
     #while True:
         #ser.open()
-        #line = port_is_waitng
-        #line = ser.readline()
-        #incomming = line.decode("UTF-8")
-        #incomming = ("Master brude være ligeglade" ";" "22"":""1"":""-1" ";" "18"":""0 "":""0" )
-        #print(incomming)
-        #Readsystem(incomming)
-        #ser.close()
+        #bytesToRead = ser.in_waiting
+        #print(bytesToRead)
+        #if (bytesToRead > 3):
+            #res = ser.read(bytesToRead)
+            #print(res)
+            #incomming = res.decode("utf-8")
+            #print(incomming)
+            #Readsystem(incomming)
+            #ser.reset_input_buffer()
+            #incomming = port.readline()
+            #print(incomming)
+
+
+
+    while True:
+        #ser.open()
+        line = ser.in_waiting
+        print(line)
+        if line > 37:
+            bytestoread = ser.read(line)
+            incomming.append(bytestoread.decode("UTF-8"))
+            #incomming = ("Master brude være ligeglade" ";" "22"":""1"":""-1" ";" "18"":""0 "":""0" )
+            print(incomming)
+            Readsystem(incomming)
+            #ser.close()
 
 
 if __name__ == "__main__":
